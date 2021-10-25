e488bbf960ae1f06d51b09df95cffb8a93fc4bb9
Casey Strong <casey_strong@byu.edu>
2021-10-24 23:20:42 -0600

    transferred files

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div class="container">
+    <div class="logo-container">
+      <img src="../assets/Title.png" >
+    </div>
+    <div class="login-container">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+.container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+}
+.login-container {
+  background: #efefef
+}
+</style>
